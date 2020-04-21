09a983366f15c5b7b8b065c7a25db38f1bd13be3
Henrique Tavares <ihenrits@gmail.com>
2020-04-20 23:53:51 -0300

    Utilizando Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
-  <BrowserRouter>
-    <Routes />
-  </BrowserRouter>
+  <>
+    <GlobalStyle />
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
+  </>
 );
 
 export default App;
