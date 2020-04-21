c07890f164c9f98fb9b4f79716675dc607d4c3d0
Henrique Tavares <ihenrits@gmail.com>
2020-04-21 20:15:28 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,72 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/27022914?s=460&u=8beafca4ba6994b2c6fc1012008a8b0c007380d9&v=4"
+            alt="Henrique Tavares"
+          />
+          <div>
+            <strong>tavareshenrique/fastfeet</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint,
+              eos?
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/27022914?s=460&u=8beafca4ba6994b2c6fc1012008a8b0c007380d9&v=4"
+            alt="Henrique Tavares"
+          />
+          <div>
+            <strong>tavareshenrique/fastfeet</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint,
+              eos?
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://avatars2.githubusercontent.com/u/27022914?s=460&u=8beafca4ba6994b2c6fc1012008a8b0c007380d9&v=4"
+            alt="Henrique Tavares"
+          />
+          <div>
+            <strong>tavareshenrique/fastfeet</strong>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint,
+              eos?
+            </p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
